34ec9cb148e61af0c237461ffe10b6648bd52e2c
fazal-a <thefazalabbas@gmail.com>
2024-01-31 05:50:45 +0500

    updated auth logic by setting userCredentials for only one time at password handshake

--- src/modules/Auth/controllers/auth.controllers.ts
+++ src/modules/Auth/controllers/auth.controllers.ts
@@ -5,10 +5,10 @@ export default {
     getAuthorizeUrl: async (req: Request, res: Response) => {
         try {
             const authorizeUrl: String = await AuthService.getAuthorizeUrl();
-            console.log('success in in getting authorizeUrl::', authorizeUrl)
+            console.log('success authorizeUrl::', authorizeUrl)
             res.status(200).json({"url": authorizeUrl});
         } catch (e: any) {
-            console.log('failure in in getting authorizeUrl error::', e)
+            console.log('failure getting authorizeUrl error::', e)
             res.status(500).send(e.message);
         }
     },
@@ -16,15 +16,14 @@ export default {
     getToken: async (req: Request, res: Response) => {
         try {
             const {code} = req.body;
-            console.log("authCode in getToken function from params code::", code)
+            console.log("authCode ::", code)
             if (code) {
                 const tokens: String = await AuthService.getToken(code);
-                console.log('success in in getting token::', tokens)
+                console.log('success token::', tokens)
                 res.status(200).json(tokens);
 
             } else {
-                console.log("in else section of getToken function of auth.controller")
-                res.status(500).send('failure and i am in else section of getToken');
+                res.status(500).send('failure in getting tokens');
 
             }
         } catch (e: any) {
--- src/modules/Auth/services/auth.services.ts
+++ src/modules/Auth/services/auth.services.ts
@@ -1,21 +1,25 @@
-import {oAuth2Client} from "../../../Shared/Utils/OAuth2Clinet";
+import { oAuth2Client } from "../../../Shared/Utils/OAuth2Clinet";
+import { scopes } from "../../../Shared/Utils/OAuth2Clinet"
+
+export let userCredential = null;
+
 export const getAuthorizeUrl = async (): Promise<any> => {
 
     return oAuth2Client.generateAuthUrl({
         access_type: 'offline',
-        scope: ['https://www.googleapis.com/auth/gmail.readonly'], // Adjust scopes based on your needs
+        scope: scopes,
+        include_granted_scopes: true
     });
 };
 
 export const getToken = async (code: string): Promise<any> => {
     try {
-            const { tokens } = await oAuth2Client.getToken(code);
-        console.log('token from the auth.services getToken function tokens::',tokens)
-            // oAuth2Client.setCredentials(tokens);
-            return tokens;
+        const { tokens } = await oAuth2Client.getToken(code);
+        oAuth2Client.setCredentials(tokens);
+        userCredential = tokens;
+        return tokens;
 
-    } catch (error:any) {
-        console.error('Error exchanging authorization code for tokens:', error.message);
+    } catch (error: any) {
         return 0;
     }
 };
