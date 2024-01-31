4f64dde1fe2220374f98a005bf8a3447b28e5441
fazal-a <thefazalabbas@gmail.com>
2024-01-31 05:55:08 +0500

    added googleapis package and updated credentials of OAuth2Clinet

--- src/Shared/Utils/OAuth2Clinet.ts
+++ src/Shared/Utils/OAuth2Clinet.ts
@@ -1,7 +1,11 @@
-import {OAuth2Client} from "google-auth-library";
+const { google } = require('googleapis');
 
-export const oAuth2Client = new OAuth2Client({
-    clientId: '858871017230-f7qoe7dpqj202r3p6ucrca3g8jrgj176.apps.googleusercontent.com',
-    clientSecret: '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',
-    redirectUri: 'http://localhost:3000/login',
-});
+export const oAuth2Client = new google.auth.OAuth2({
+  clientId: '858871017230-f7qoe7dpqj202r3p6ucrca3g8jrgj176.apps.googleusercontent.com',
+  clientSecret: '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',
+  redirectUri: 'http://localhost:3000/login',
+}
+);
+export const scopes = [
+  'https://www.googleapis.com/auth/gmail.readonly'
+];
